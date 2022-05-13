9189466598bdb422648188b0c7189bb5e2cf7a3f
Timur <100406268+Timik232@users.noreply.github.com>
2022-05-13 16:24:18 +0300

    finished

--- siaode7/siaode7.cpp
+++ siaode7/siaode7.cpp
@@ -1,26 +1,124 @@
 ﻿// siaode7.cpp : вариант 13
 //Префиксная	Массив	Вычислить значение выражения
 #include <iostream>
+#include <string>
+#include <math.h>
 using namespace std;
 
-void in_to_mas(string prefix,char a[100])
+int in_to_mas(string prefix,char a[100])
 {
-	int count = 0;
-	for (int i = 0; i < prefix.size(); i++)
+	int n = prefix.size();
+	for (int i = 0; i < n; i++)
 	{
-		if (prefix[i] != ' ')
+		a[i] = prefix[i];		
+	}
+	return n;
+}
+float counting(string prefix)
+{
+	float a[100];
+	int n = prefix.size();
+	string buf = "";
+	int count = -1;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (prefix[i] == '+')
 		{
-			a[count] = prefix[i];
-			count += 1;
+			if (buf != "")
+			{
+				count += 1;
+				//cout << "buf=" << buf << " ";
+				a[count] = stof(buf);
+				//cout << "a=" << a[count] << " ";
+				buf = "";
+			}
+			float cifr1 = a[count];
+			a[count] = 0;
+			count -= 1;
+			float cifr2 = a[count];
+			a[count] = cifr1 + cifr2;
+		}
+		else if (prefix[i] == '-')
+		{
+			if (buf != "")
+			{
+				count += 1;
+				//cout << "buf=" << buf << " ";
+				a[count] = stof(buf);
+				//cout << "a=" << a[count] << " ";
+				buf = "";
+			}
+			float cifr1 = a[count];
+			a[count] = 0;
+			count -= 1;
+			float cifr2 = a[count];
+			a[count] = cifr1 - cifr2;
+		}
+		else if (prefix[i] == '*')
+		{
+			if (buf != "")
+			{
+				count += 1;
+				//cout << "buf=" << buf << " ";
+				a[count] = stof(buf);
+				//cout << "a=" << a[count] << " ";
+				buf = "";
+			}
+			float cifr1 = a[count];
+			a[count] = 0;
+			count -= 1;
+			float cifr2 = a[count];
+			a[count] = cifr1 * cifr2;
+		}
+		else if (prefix[i] == '/')
+		{
+			if (buf != "")
+			{
+				count += 1;
+				//cout << "buf=" << buf << " ";
+				a[count] = stof(buf);
+				//cout << "a=" << a[count] << " ";
+				buf = "";
+			}
+			float cifr1 = a[count];
+			a[count] = 0;
+			count -= 1;
+			float cifr2 = a[count];
+			a[count] = cifr1 / cifr2;
+		}
+		else if (prefix[i] == '^')
+		{
+			float cifr1 = a[count];
+			a[count] = 0;
+			count -= 1;
+			float cifr2 = a[count];
+			a[count] = pow(cifr1, cifr2);
+		}
+		else if (prefix[i] != ' ')
+			buf += prefix[i];
+		else if (prefix[i] == ' ')
+		{
+			if (buf != "")
+			{
+				count += 1;
+				//cout << "buf=" << buf << " ";
+				a[count] = stof(buf);
+				//cout << "a=" << a[count] << " ";
+				buf = "";
+			}
 		}
 	}
+	return a[count];
 }
-
 int main()
 {
 	string prefix = "-*+2 3 5 * 2 3";
 	string prefix1 = "+3 * 4 2";
-	char a[100];
-	in_to_mas(prefix,a);
-	cout << a[1];
+	//char a[100];
+	//int n = in_to_mas(prefix,a);
+	cout << "first result: " << counting(prefix1) << endl;
+	//char b[100];
+	//n = in_to_mas(prefix1, b);
+	cout << "second result: " << counting(prefix);
+	system("pause");
 }
